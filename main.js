530e601b2cb17f3fa5b7411e4fd3d6626de8b088
sabbakilam <pitdrop@gmail.com>
2016-04-04 19:59:35 +0000

    captures playlists

--- main.js
+++ main.js
@@ -1,24 +1,58 @@
 //---data--
 var gitname = document.getElementById("gitname");
-var btn = document.getElementById("btn")
+var btn = document.getElementById("btn");
+var chooser = document.getElementById("chooser");
 var lists = {};
+var namesArray = [];
+var ajax = new XMLHttpRequest();
 //---event handlers----
 window.onload = initialize;
 //-----functions-------
 function initialize(){
-   // makeSelectionBox();
     btn.onclick = function(){
         //point to url
         var url = "https://" + gitname.value + ".github.io/music/list.json";
-        var ajax = new XMLHttpRequest();
+
         ajax.open("GET", url);
         ajax.send();
         //------------
         ajax.onload = function(){
-            alert(ajax.response);
-            //add to lists
-            var newname = gitname.value.trim();
-            lists[newname] = JSON.parse(ajax.response);
+            if(ajax.status === 200){
+                saveNewList();
+            }
+            else{
+                alert("Trouble getting list:\n" + ajax.response);
+            }
         };
-    };  
+    };
 }//===| END of initialize() |=====
+function addToBox(newGitName){
+    
+    //make a real array of options from chooser
+    var opsArray = [].slice.call(chooser.options, 0);
+    namesArray = [];
+    opsArray.forEach(function(m){
+        namesArray.push(m.innerHTML);
+
+    });
+
+    //add newGitName only if not aready there
+    if(namesArray.indexOf(newGitName) === -1){
+        var op = document.createElement("option");
+        op.innerHTML = newGitName;
+        chooser.appendChild(op);
+    }
+}
+
+function sendListToServer(object){
+    var objectString = JSON.stringify(object);
+}
+
+function saveNewList(){
+    var newname = gitname.value.toLowerCase().trim();
+    if(!lists[newname]){
+        lists[newname] = JSON.parse(ajax.response);
+        addToBox(newname);
+        sendListToServer(lists);
+    }
+}
